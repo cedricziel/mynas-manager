bb3afcf325bc9b0f9528e48130c67054e57fe082
Cedric Ziel <cedric@cedric-ziel.com>
2025-07-19 20:25:01 +0200

    cleanup: remove template boilerplate files
    - Removed bin/mynas_backend.dart (template file)
    - Removed lib/mynas_backend.dart (template library)
    - Removed test/mynas_backend_test.dart (template test)
    - Kept mock_truenas_server.dart and test_with_mock.dart for development
    - Production backend only needs bin/server.dart executable

--- backend/bin/mynas_backend.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mynas_backend/mynas_backend.dart' as mynas_backend;
-
-void main(List<String> arguments) {
-  print('Hello world: ${mynas_backend.calculate()}!');
-}
--- backend/test/mynas_backend_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mynas_backend/mynas_backend.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
